fc8ada7deb4fe0a80d727724ce715fce7616a21f
Alberto Quiles Boix <alberto.quil3s@gmail.com>
2025-05-11 20:09:34 +0200

    wip

--- src/client/game.tsx
+++ src/client/game.tsx
@@ -3,8 +3,9 @@ import { GameModule } from "@oh/utils";
 import { GameComponent } from "./modules/game.component";
 import { GameProvider } from "./shared/hooks/game/game.provider";
 
+
 export const game: GameModule = {
-  id: "game-template",
+  id: "game-template123",
   version: "0.0.1",
   name: "Game Template",
   start: ({ data, onEnd }) => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,46 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   clearScreen: false,
-  plugins: [react(), reactRefresh(), tsconfigPaths()],
+  plugins: [react()],
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
-  root: "./src/client",
-  base: "/",
-  publicDir: "./assets/",
+  // root: "./src/client",
+  // base: "/",
+  // publicDir: "./assets/",
   build: {
     lib: {
-      entry: "./game.tsx",
-      fileName: "bundle",
+      entry: "./src/client/game.tsx",
+      fileName: (format) => `bundle.js`,
       formats: ["es"],
     },
-    outDir: "../../build",
-    minify: false,
+    outDir: "./build",
+    minify: true,
     emptyOutDir: true,
     rollupOptions: {
       external: [
         // "react",
-        // "react-dom",
-        //   "@openhotel/pixi-components",
-        //   "@oh/utils",
+        //     // "react-dom",
+        //     // "@openhotel/pixi-components",
+        //     // "@oh/utils",
       ],
       output: {
+        inlineDynamicImports: false,
+        format: "module",
         manualChunks: () => "everything",
+        // globals: {
+        //   react: "reactu",
+        // },
       },
     },
   },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern",
-      },
-    },
-  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       api: "modern",
+  //     },
+  //   },
+  // },
 });
